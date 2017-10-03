cbf758e7d8c49dba030f4fda39257cfa6d7b3994
John Wang <johncwang@gmail.com>
2017-10-03 12:28:09 -0700

    add get_transcript example

---
+++ examples/get_transcript/get_transcript.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/grokify/go-voicebase-v3"
+	"github.com/grokify/gotilla/fmt/fmtutil"
+	"github.com/joho/godotenv"
+)
+
+const (
+	EnvAccessToken       = "VOICEBASE_BEARER_TOKEN"
+	EnvMediaId           = "VOICEBASE_TRANSCRIPT_MEDIA_ID"
+	VoiceBaseApiBasePath = "https://apis.voicebase.com"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	accessToken := os.Getenv(EnvAccessToken)
+	mediaId := os.Getenv(EnvMediaId)
+
+	mediaApi := voicebase.NewMediaApiWithBasePath(VoiceBaseApiBasePath)
+	mediaApi.Configuration.SetAccessToken(accessToken)
+
+	vbt, resp, err := mediaApi.GetTranscript(mediaId, []string{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(resp.Payload))
+
+	fmtutil.PrintJSON(vbt)
+
+	fmt.Println("DONE")
+}
