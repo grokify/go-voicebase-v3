d9c1d5a2df8d18c117696e90bb5d732048d3da6d
John Wang <johncwang@gmail.com>
2018-01-03 00:09:49 -0800

    update style

--- examples/get_media/get_media.go
+++ examples/get_media/get_media.go
@@ -3,19 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/grokify/go-voicebase-v3"
 	"github.com/grokify/gotilla/fmt/fmtutil"
-	"github.com/grokify/gotilla/net/httputilmore"
 	"github.com/grokify/oauth2more"
 	"github.com/joho/godotenv"
-)
-
-const (
-	EnvAccessToken = "VOICEBASE_BEARER_TOKEN"
 )
 
 func LoadEnv() error {
@@ -25,62 +19,33 @@ func LoadEnv() error {
 	return godotenv.Load()
 }
 
-func getMediaHttpClient(client *http.Client) (*voicebase.VbMediaQueryResponse, error) {
-	resp, err := client.Get("https://apis.voicebase.com/v3/media")
-	if err != nil {
-		return nil, err
-	}
-
-	err = httputilmore.PrintResponse(resp, true)
-	if err != nil {
-		return nil, err
-	}
-
-	mediaResponse := &voicebase.VbMediaQueryResponse{}
-
-	err = httputilmore.UnmarshalResponseJSON(resp, mediaResponse)
-
-	return mediaResponse, err
-}
-
-func getMediaApiClient(client *http.Client) (*voicebase.VbMediaQueryResponse, error) {
-	apiConfig := voicebase.NewConfiguration()
-	apiConfig.HTTPClient = client
-
-	apiClient := voicebase.NewAPIClient(apiConfig)
-
-	mediaResponse, resp, err := apiClient.MediaApi.MediaQuery(context.Background(), map[string]interface{}{})
-	if err != nil {
-		return nil, err
-	} else if resp.StatusCode > 299 {
-		return nil, fmt.Errorf("VoiceBase media query returned status %v", resp.StatusCode)
-	}
-	return &mediaResponse, nil
-}
-
 func main() {
 	err := LoadEnv()
 	if err != nil {
 		panic(err)
 	}
 
-	client := oauth2more.NewClientAccessToken(os.Getenv(EnvAccessToken))
+	httpClient := oauth2more.NewClientAccessToken(
+		os.Getenv("VOICEBASE_BEARER_TOKEN"),
+	)
+	apiConfig := voicebase.NewConfiguration()
+	apiConfig.HTTPClient = httpClient
+	apiClient := voicebase.NewAPIClient(apiConfig)
 
-	useApiClient := true
-
-	if useApiClient {
-		mediaResponse, err := getMediaApiClient(client)
-		if err != nil {
-			panic(err)
-		}
-		fmtutil.PrintJSON(mediaResponse)
-	} else {
-		mediaResponse, err := getMediaHttpClient(client)
-		if err != nil {
-			panic(err)
-		}
-		fmtutil.PrintJSON(mediaResponse)
+	mediaResponse, resp, err := apiClient.MediaApi.MediaQuery(
+		context.Background(),
+		map[string]interface{}{},
+	)
+	if err != nil {
+		panic(err)
+	} else if resp.StatusCode > 299 {
+		panic(fmt.Errorf(
+			"VoiceBase media query returned status %v",
+			resp.StatusCode),
+		)
 	}
+
+	fmtutil.PrintJSON(mediaResponse)
 
 	fmt.Println("DONE")
 }
